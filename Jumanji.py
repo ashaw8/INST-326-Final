7dfb9265484c84f01c75c9eddc47f35ae933dd26
Jeffrey Gomes <jgomes12@terpmail.umd.edu>
2022-11-01 19:49:30 -0400

    Added my name and a few more lines to the docstring above detailing what our project would be like and a little bit of what the capabilities would be apart of the game. I also built a skeleton of an example list. Feel free to edit.

--- Jumanji.py
+++ Jumanji.py
@@ -1,4 +1,12 @@
 """Jumanji Game Board
 11/1/2022
-Authors: Aidan Shawyer, 
+Authors: Aidan Shawyer, Jeffrey Gomes
+
+This is a starter doctsring saying who is exactly in our
+group for this final project. In which we will be making
+our own version of the Jumanji board game. It will consist
+of different players and different types of actions which 
+the board will play on the user or the player themselves
+will play. But this is an example intro docstring.
 """
+example_list= ()
