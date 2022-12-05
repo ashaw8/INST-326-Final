373ec26186d62bbb9ccc00e293ad96c6adb5fd47
Aidan Shawyer <ashawyer@umd.edu>
2022-12-05 17:19:55 -0500

    regex

--- MASH.py
+++ MASH.py
@@ -4,4 +4,19 @@ import sys
 
 
 def categories(textline):
-    expression = r"""(?P<name>^[A-Z]{1}\w+,\s[A-Z]{1}\w+)\s(?P<kids>\d+)\s(?P<transportation>[A-Z]{1}[a-z]+)\s(?P<pet>[a-z]+)\s(?P<place>[A-Z][a-z]+)"""
+    expression = r'''(?P<name>^[A-Z]{1}\w+,\s[A-Z]{1}\w+)\s(?P<kids>\d+)
+    \s(?P<transportation>[A-Z]{1}[a-z]+)
+    \s(?P<pet>[a-z]+)\s(?P<place>[A-Z][a-z]+)'''
+    for i in textline:
+        print(i)
+        search_regex = re.search(expression, i)
+    
+  
+random_list = []  
+with open("MASH.txt","r", encoding="utf-8") as f:
+    for line in f:
+        line = line.strip()
+        random_list.append(line)
+categories(random_list)
+    
+    
