84eab952edb7c3b66de4b908121662f8f1ca9036
Aidan Shawyer <ashawyer@umd.edu>
2022-12-05 21:11:40 -0500

    Random select from MASH

--- MASH.py
+++ MASH.py
@@ -4,22 +4,49 @@ import sys
 
 
 
-def categories(textline):
+def categories(textline, option):
     expression = r'''(?P<name>^[A-Z]{1}\w+,\s[A-Z]{1}\w+)\s(?P<kids>\d+)\s(?P<transportation>[A-Z]{1}[a-z]+)\s(?P<pet>[a-z]+)\s(?P<place>[A-Z][a-z]+)'''
-    
     search_regex = re.search(expression,textline)
-    return search_regex.group(2)
+    if option == 1:
+        name = search_regex.group(1)
+        return name
+    if option == 2:
+        kids = search_regex.group(2)
+        return kids
+    if option == 3:
+        car = search_regex.group(3)
+        return car
+    if option == 4:
+        pet = search_regex.group(4)
+        return pet
+    if option == 5:
+        location = search_regex.group(5)
+        return location
     
     
-def separate(textline):
-        name = list(filter(lambda v: re.match('[A-Z][a-z]+,\s[A-Z][a-z]+',v)))
-        kids = list(filter())
-  
-random_list = []  
-with open("MASH.txt","r", encoding="utf-8") as f:
-    for line in f:
-        line = line.strip()
-        random_list.append(categories(line))
-print(random_list)
-    
-    
+
+def open_file(textfile):  
+    random_list = []  
+    with open(textfile,"r", encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
+            random_list.append(line)
+    return random_list
+
+def pass_regex():
+    stored = open_file("MASH.txt")
+    list1 = []
+    list2 = []
+    list3 = []
+    list4 = []
+    list5 = []
+    for line in stored:
+        list1.append(categories(line, 1))
+        list2.append(categories(line, 2))
+        list3.append(categories(line, 3))
+        list4.append(categories(line, 4))
+        list5.append(categories(line, 5))
+        
+    return [random.choice(list1),random.choice(list2),random.choice(list3),random.choice(list4),random.choice(list5),]
+
+print(pass_regex())
