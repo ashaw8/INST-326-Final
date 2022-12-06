263e60723d17feb923b57083781f8d7a74f6521e
Peter Mensah <81762687+pjmensahh@users.noreply.github.com>
2022-12-06 12:31:06 -0500

    main

--- MASH.py
+++ MASH.py
@@ -44,5 +44,15 @@ def pass_regex():
         
     return [random.choice(list1),random.choice(list2),random.choice(list3),random.choice(list4),random.choice(list5)]
 
-print(pass_regex())
 
+def main():
+    regex = pass_regex()
+    print(f'''Your partner is {regex[0]}\n
+          and you will have {regex[1]} kids\n
+          and you get around by {regex[2]}\n
+          your pet is a {regex[3]}\n
+          and you live in {regex[4]}''')
+    
+    
+    
+main()
